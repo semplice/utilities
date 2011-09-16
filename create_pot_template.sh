58adba1804d0a453c983f1344de35e9033dde9ed
g7 <morarossa@gmail.com>
2011-09-16 17:18:31 +0200

    * semplice-logout: switched to a .ini configuration file that resiedes   in ~/.semplice-logout * semplice-logout: added working "Settings" dialog * semplice-logout: ability to not show confiirmation window.

--- create_pot_template.sh
+++ create_pot_template.sh
@@ -8,7 +8,14 @@
 
 APP_NAME="semplice-utilities"
 
+# Extract strings from logout.glade
+intltool-extract --type=gettext/glade logout.glade
+
 # Extract string from semplice-about and semplice-logout
-xgettext --language="Python" --keyword=_ --output=lang/$APP_NAME/$APP_NAME.pot semplice-about semplice-logout
+xgettext --language="Python" --keyword=_ --output=lang/$APP_NAME/$APP_NAME.pot semplice-about semplice-logout logout.glade.h
 # Extract string from nitrogen-add-wallpaper
 xgettext --output=lang/$APP_NAME/$APP_NAME.pot -j --language=Shell nitrogen-add-wallpaper
+
+xgettext --language="C" --keyword=N_ -j --output=lang/$APP_NAME/$APP_NAME.pot -j logout.glade.h
+
+rm logout.glade.h
